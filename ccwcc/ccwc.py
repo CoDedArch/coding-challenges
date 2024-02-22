894b03271d493b84c3c56b5aaa9274e9ec29ab72
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-02-22 14:48:44 +0000

    method to return default options provided no option is given to the program

--- ccwcc/ccwc.py
+++ ccwcc/ccwc.py
@@ -16,34 +16,50 @@ def count_words(file_name):
 def count_chars(file_name):
     with open(file_name, 'r', encoding='utf-8') as f:
         return sum(len(line) for line in f)
+    
+def default_options(file_name):
+    byte_count = count_bytes(file_name)
+    word_count = count_words(file_name)
+    line_count = count_lines(file_name)
+    char_count = count_chars(file_name)
+
+    print(f'{line_count} {word_count} {byte_count} {file_name}')
+    sys.exit()
 
 def main(argv):
     file_name = None
     option = None
+    print(argv)
 
-    try:
-        opts, args = getopt(argv, 'c:i:w:m:', ['byte=', 'line=', 'word=', 'char='])
-    except GetoptError:
-        print(f'{__name__} -c <command> failed')
-        sys.exit(2)
+    if len(argv) == 1:
+            file_name = argv[0]
+    if not len(argv) == 1:
+        try:
+            opts, args = getopt(argv, 'c:i:w:m: ', ['byte=', 'line=', 'word=', 'char='])
+        except GetoptError:
+            print(f'{__name__} -c <command> failed')
+            sys.exit(2)
 
-    if not opts:
-        print(f"Usage: {__name__} -c <command> <filename>")
-        sys.exit(2)
+        if not opts:
+            print(f"Usage: {__name__} -c <command>")
+            sys.exit(2)
 
-    option, file_name = opts[0]
-    print(file_name)
+        option, file_name = opts[0]
+        print(file_name)
 
-    if option in ('-c', '--byte'):
-        result = count_bytes(file_name)
-    elif option in ('-i', '--line'):
-        result = count_lines(file_name)
-    elif option in ('-w', '--word'):
-        result = count_words(file_name)
-    elif option in ('-m', '--char'):
-        result = count_chars(file_name)
+        if option in ('-c', '--byte'):
+            result = count_bytes(file_name)
+        elif option in ('-i', '--line'):
+            result = count_lines(file_name)
+        elif option in ('-w', '--word'):
+            result = count_words(file_name)
+        elif option in ('-m', '--char'):
+            result = count_chars(file_name)
+        print(f'{result} {file_name}')
+        sys.exit()
 
-    print(f'{result} {file_name}')
+    
+    default_options(file_name)
 
 if __name__ == '__main__':
     main(sys.argv[1:])

141bd94b64c994dfac193aa7d23330de2f8324da
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-03-10 23:22:00 +0000

    wrote test for function

--- DailyByte/StringReversal.py
+++ DailyByte/StringReversal.py
@@ -1,6 +1,23 @@
-def stringReversal(input_string:str):
+import unittest
+
+def stringReversal(input_string):
    if type(input_string) is not str:
       raise ValueError('function argument must be a string')
    return input_string[::-1]
 
 print(stringReversal('Hear am i'))
+
+
+# unittest 
+class TestStringReversal(unittest.TestCase):
+   def test_string_reversal(self):
+      self.assertEqual(stringReversal('Cat'), 'taC')
+      self.assertEqual(stringReversal('Dog'), 'goD')
+      self.assertEqual(stringReversal(''), '')
+
+   def test_string_reversal_error(self):
+      with self.assertRaises(ValueError):
+         stringReversal(123)
+
+if __name__ == '__main__':
+   unittest.main()

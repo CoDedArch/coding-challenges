52e9b268fd9090e4b4d50edc9bd2e8c21a944374
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-03-15 00:26:54 +0000

    done with class

--- DailyByte/correct-capitalization.py
+++ DailyByte/correct-capitalization.py
@@ -1,3 +1,7 @@
+name = 'keL'
+print (name.split())
+
+
 class CorrectCapitalization:
     """This Class Answers Google Questions
         on testing whether a string uses correct capitalization
@@ -14,6 +18,8 @@ class CorrectCapitalization:
         """
         self.string_input = input_string
         self.cap_conditions = []
+        self.is_correctly_capitalized = None
+
     def check_all_caps(self, string_to_check:str):
         letters_all_caps = True
         while letters_all_caps:
@@ -21,6 +27,32 @@ class CorrectCapitalization:
                 if letter is not letter.isupper():
                     letters_all_caps = False
                     break
-        return letters_all_caps                
-    def set_cap_conditions(self):
-        self.cap_conditions.append(self.check_all_caps(self.string_input))      
+        return letters_all_caps
+    def check_for_no_caps(self, string_to_check:str):
+        return string_to_check.islower()
+    
+    def only_first_is_cap(self, string_to_check:str):
+        first_letter_cap = True
+        remaining_letters_not_cap = True
+        first_letter= string_to_check[0]
+        remaining_letters = string_to_check[1:]
+
+        if not first_letter.isupper():
+            first_letter_cap = False
+        
+        if not self.check_for_no_caps(remaining_letters):
+            remaining_letters_not_cap = False
+        return all([first_letter_cap, remaining_letters_not_cap])
+    
+    def set_cap_conditions(self, string_to_check):
+        self.cap_conditions.append(self.check_all_caps(string_to_check))
+        self.cap_conditions.append(self.check_for_no_caps(string_to_check))
+        self.cap_conditions.append(self.only_first_is_cap(string_to_check))
+        return 
+
+    def check_is_correctly_cap(self):
+        self.set_cap_conditions(self.string_input)
+        self.is_correctly_capitalized = all(self.cap_conditions)
+        return self.is_correctly_capitalized
+
+

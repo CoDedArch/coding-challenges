f7573695d3393b29fe7949a4fc6897fbaf384001
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-03-29 15:19:21 +0000

    inial logic to implement trie pattern matching

---
+++ DailyByte/trie_of_patterns.py
@@ -0,0 +1,20 @@
+class TriePatterns:
+    def __init__(self,patterns:list) -> None:
+            self.patterns = patterns
+
+def set_patterns(length_of_patterns):
+     i = 1
+     patterns = []
+     while i <= length_of_patterns:
+          pattern = input('enter the genome pattern allowable characters are: (A,C,G,T): ')
+
+def confirm_valid_pattern(pattern):
+     pass
+     
+is_in_range = True
+while(is_in_range):
+    n = int(input('enter the length of your parttens: '))
+    if n > 0 and n <= 100:
+        is_in_range = False
+
+

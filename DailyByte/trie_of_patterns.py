1ec636de1540a8bb841c9c12147939924bac9644
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-03-30 22:57:09 +0000

    Add graph formation logic to TriePatterns class

--- DailyByte/trie_of_patterns.py
+++ DailyByte/trie_of_patterns.py
@@ -2,6 +2,35 @@ class TriePatterns:
     def __init__(self,n:int, patterns:list) -> None:
             self.patterns = patterns
             self.n = n
+            self.graph = {}
+    def form_graph(self):
+        # add root
+        self.graph['0'] = set()
+        for pi in self.patterns:
+            self.graph['0'].add(pi[0])
+            # for edges as we go
+            self.graph[pi[0]] = set()
+            self.add_edge(pi)
+           
+        print(self.graph)
+    
+    def add_edge(self, genome):
+        
+        if len(genome) < 2: 
+            print(genome)
+            return
+
+        initial_prefix, next_char = genome[0], genome[1]
+        if initial_prefix in self.graph:
+            self.graph[initial_prefix].add(next_char)
+        else:
+            print('Key does not exist')
+            self.graph[initial_prefix] = {next_char}
+        
+        if next_char not in self.graph:
+            self.graph[next_char] = set()
+
+        return self.add_edge(genome[1:])
 
     def set_patterns(self):
         i = 1
@@ -26,7 +55,6 @@ class TriePatterns:
         return set(pattern) <= {'A', 'C', 'G', 'T'} and not is_prefix
 
 
-
 is_in_range = True
 while(is_in_range):
     n = int(input('enter the length of your parttens: '))
@@ -36,3 +64,4 @@ while(is_in_range):
 
 test_pattern = TriePatterns(n=n, patterns=[])
 test_pattern.set_patterns()
+test_pattern.form_graph()

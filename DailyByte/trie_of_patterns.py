105833bfb8d0309cfdf205c6579fadf3d2a7193e
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-03-30 23:38:18 +0000

    Refactor Trie implementation and add pattern insertion and display methods

--- DailyByte/trie_of_patterns.py
+++ DailyByte/trie_of_patterns.py
@@ -1,58 +1,54 @@
-class TriePatterns:
-    def __init__(self,n:int, patterns:list) -> None:
-            self.patterns = patterns
-            self.n = n
-            self.graph = {}
-    def form_graph(self):
-        # add root
-        self.graph['0'] = set()
-        for pi in self.patterns:
-            self.graph['0'].add(pi[0])
-            # for edges as we go
-            self.graph[pi[0]] = set()
-            self.add_edge(pi)
-           
-        print(self.graph)
-    
-    def add_edge(self, genome):
-        
-        if len(genome) < 2: 
-            print(genome)
-            return
+patterns = []
+class TrieNode:
+    def __init__(self, char: str):
+        self.char = char
+        self.children = {}
 
-        initial_prefix, next_char = genome[0], genome[1]
-        if initial_prefix in self.graph:
-            self.graph[initial_prefix].add(next_char)
+class Trie:
+    def __init__(self):
+        self.root = TrieNode("")
+
+    def insert(self, word: str):
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                new_node = TrieNode(char)
+                node.children[char] = new_node
+                node = new_node
+
+    def dfs(self, node, prefix):
+        if node.children:
+            for char, child in node.children.items():
+                self.dfs(child, prefix + char)
         else:
-            print('Key does not exist')
-            self.graph[initial_prefix] = {next_char}
-        
-        if next_char not in self.graph:
-            self.graph[next_char] = set()
+            print(prefix)
 
-        return self.add_edge(genome[1:])
+    def display(self):
+        self.dfs(self.root, "")
 
-    def set_patterns(self):
-        i = 1
-        self.patterns = []
-        while i <= self.n:
-            is_a_valid_pattern = False
-            while not is_a_valid_pattern:
-                pattern = input('enter the genome pattern; allowable characters are: (A,C,G,T): ')    
-                is_a_valid_pattern = self.confirm_valid_pattern(pattern)
-            self.patterns.append(pattern)
-            if self.n-i > 0:
-                print(f'Genome valid and accepted-------> remaining {self.n-i} genome patterns')
-            i += 1
-        return
 
-    def confirm_valid_pattern(self, pattern:str):
-        is_prefix = False
-        for pi in self.patterns:
-            is_prefix = pattern.startswith(pi)
-            if is_prefix:
-                break
-        return set(pattern) <= {'A', 'C', 'G', 'T'} and not is_prefix
+def set_patterns(n):
+    i = 1
+    while i <= n:
+        is_a_valid_pattern = False
+        while not is_a_valid_pattern:
+            pattern = input('enter the genome pattern; allowable characters are: (A,C,G,T): ')    
+            is_a_valid_pattern = confirm_valid_pattern(pattern)
+        patterns.append(pattern)
+        if n-i > 0:
+            print(f'Genome valid and accepted-------> remaining {n-i} genome patterns')
+        i += 1
+    return
+
+def confirm_valid_pattern(pattern:str):
+    is_prefix = False
+    for pi in patterns:
+        is_prefix = pattern.startswith(pi)
+        if is_prefix:
+            break
+    return set(pattern) <= {'A', 'C', 'G', 'T'} and not is_prefix
 
 
 is_in_range = True
@@ -61,7 +57,9 @@ while(is_in_range):
     if n > 0 and n <= 100:
         is_in_range = False
 
+set_patterns(n=n)
 
-test_pattern = TriePatterns(n=n, patterns=[])
-test_pattern.set_patterns()
-test_pattern.form_graph()
+trie = Trie()
+for pattern in patterns:
+    trie.insert(pattern)
+trie.display()
